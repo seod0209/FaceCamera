fb37b4beb9a0bfd41f897f9847ce00f083965e21
seod0209 <don9zseo@gmail.com>
2024-08-25 16:55:24 +0900

    feat: initial setting - add libraries - set each config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    '@react-native',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-indexed-object-style': 'off',
+    '@typescript-eslint/no-confusing-void-expression': [
+      'error',
+      {
+        ignoreArrowShorthand: true,
+        ignoreVoidOperator: true,
+      },
+    ],
+  },
 };
